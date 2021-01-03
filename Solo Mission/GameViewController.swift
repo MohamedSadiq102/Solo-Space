91e7b6eaae3a8e0c1d7f3757ae9d4c5d78da82ee
Mohamed Sadiq <mohamed.sadiq@fiware.org>
2021-01-03 07:34:16 +0100

    Adding StartMenu and BackingAudio

--- Solo Mission/GameViewController.swift
+++ Solo Mission/GameViewController.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+//import GameplayKit
+import AVFoundation
 
 class GameViewController: UIViewController {
 
+    // back in Audio, globaly, in order to not stop
+    var backingAudio = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let filePath = Bundle.main.path(forResource: "backingAudio", ofType: "mp3")
+        let audioNSURL = NSURL(fileURLWithPath: filePath!)
+        do { backingAudio = try AVAudioPlayer(contentsOf: audioNSURL as URL)}
+        catch { return print("Can't Find The Audio") }
+        // -1 is forever playing
+        backingAudio.numberOfLoops = -1
+        backingAudio.volume = 1
+        backingAudio.play()
+        
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
 //            if let scene = SKScene(fileNamed: "GameScene") {
-            let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+            let scene = MainMenuScene(size: CGSize(width: 1536, height: 2048))
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
---
+++ Solo Mission/MainMenuScene.swift
@@ -0,0 +1,75 @@
+//
+//  MainMenuScene.swift
+//  Solo Mission
+//
+//  Created by Mohamedsadiq on 03.01.21.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        // when the i = 0, the foto will be buttom of the screen, when 1 at the up of the screen
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+        let gameBy = SKLabelNode(fontNamed: "theboldfont")
+        gameBy.text = "Mo Sadiq"
+        gameBy.fontSize = 60
+        gameBy.fontColor = SKColor.white
+        gameBy.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.78)
+        gameBy.zPosition = 1
+        self.addChild(gameBy)
+        
+        let gameName1 = SKLabelNode(fontNamed: "theboldfont")
+        gameName1.text = "Solo"
+        gameName1.fontSize = 200
+        gameName1.fontColor = SKColor.white
+        gameName1.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.7)
+        gameName1.zPosition = 1
+        self.addChild(gameName1)
+        
+        let gameName2 = SKLabelNode(fontNamed: "theboldfont")
+        gameName2.text = "Mission"
+        gameName2.fontSize = 200
+        gameName2.fontColor = SKColor.white
+        gameName2.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.625)
+        gameName2.zPosition = 1
+        self.addChild(gameName2)
+        
+        let startGame = SKLabelNode(fontNamed: "theboldfont")
+        startGame.text = "Start Game"
+        startGame.fontSize = 150
+        startGame.fontColor = SKColor.white
+        startGame.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.4)
+        startGame.zPosition = 1
+        startGame.name = "startButton"
+        self.addChild(startGame)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch: AnyObject in touches {
+            
+            let pointOftouch = touch.location(in: self)
+            let nodeITapped = atPoint(pointOftouch)
+            
+            if nodeITapped.name == "startButton"{
+                
+                let sceneToMoveTo = GameScene(size: self.size)
+                sceneToMoveTo.scaleMode = self.scaleMode
+                let myTransition = SKTransition.fade(withDuration: 0.5)
+                self.view!.presentScene(sceneToMoveTo, transition: myTransition)
+                
+                
+            }
+        }
+        
+    }
+}
